923b77e080dec92f4bb1c3bd47d9932789b1b5e3
Tamara89-M <tamara.muradyan04@gmail.com>
2022-07-17 12:01:43 +0400

    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

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,12 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        int result = Math.round(numberToBeRounded);
+        System.out.println(result);
+    }
+
+    public static void main(String[] args) {
+        FloatTypeCasting floatTypeCasting = new FloatTypeCasting();
+        floatTypeCasting.roundNumber(9.4f);
     }
 }
